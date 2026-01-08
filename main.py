54213dcd3bc5a6d07950f472c3422235a25a4947
Zyberfenn <camtopp@gmail.com>
2026-01-08 09:23:18 -0500

    Base LLM made

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import os
+import argparse
 from dotenv import load_dotenv
 
 from google import genai
+from google.genai import types
 
 load_dotenv()
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+messages = [types.Content(
+    role="user", 
+    parts=[types.Part(text=args.user_prompt)]
+    )]
+
 api_key = os.environ.get("GEMINI_API_KEY")
-if api_key == None:
+if api_key is None:
     raise RuntimeError("No API Key Found.")
 
 client = genai.Client(api_key=api_key)
 
+
 response = client.models.generate_content(
     model='gemini-2.5-flash', 
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    contents= messages
 )
 
-if response.usage_metadata == None:
+if response.usage_metadata is None:
     raise RuntimeError("No Usage Metadata Found.")
 
-print(f"User Prompt: {response.contents}")
-print(f"Prompt Tokens: {response.usage_metadata.prompt_token_count}")
-
-
-print(response.text)
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(f"Response: {response.text}")
